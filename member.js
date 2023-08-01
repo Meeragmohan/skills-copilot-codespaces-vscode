50d0f05b35daba862459c2d41e36ea9d9c123427
Meera Mohan <meeragmohan@gmail.com>
2023-08-01 04:42:48 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsmeber() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberbtn = document.getElementById("memberbtn");
+    var skillsbtn = document.getElementById("skillsbtn");
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberbtn.style.backgroundColor = "#fff";
+    skillsbtn.style.backgroundColor = "#f1f1f1";
+}
